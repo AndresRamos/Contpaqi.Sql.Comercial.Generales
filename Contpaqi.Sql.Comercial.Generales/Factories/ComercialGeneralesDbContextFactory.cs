f47d7bcc35ceb81d7ec4954eabed8ceb5d0bcb43
Andres Ramos <andres@arsoft.net>
2020-08-03 12:14:55 -0500

    Version 1.0.5 Se actualizo el modelo de datos para representar la version 6.0.2 de CONTPAQi Comercial

---
+++ Contpaqi.Sql.Comercial.Generales/Factories/ComercialGeneralesDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Contpaqi.Sql.Comercial.Generales.Factories
+{
+    public static class ComercialGeneralesDbContextFactory
+    {
+        public const string NombreBaseDatos = "CompacWAdmin";
+
+        public static ComercialGeneralesDbContext CreateInstance(string contpaqiConnectionString)
+        {
+            if (contpaqiConnectionString == null)
+            {
+                throw new ArgumentNullException(nameof(contpaqiConnectionString));
+            }
+
+            var connectionStringBuilder = new SqlConnectionStringBuilder(contpaqiConnectionString)
+            {
+                InitialCatalog = NombreBaseDatos
+            };
+
+            return new ComercialGeneralesDbContext(new SqlConnection(connectionStringBuilder.ToString()), true);
+        }
+    }
+}
